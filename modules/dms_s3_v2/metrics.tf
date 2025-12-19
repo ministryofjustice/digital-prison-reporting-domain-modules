99fe51d1624274388fc15236b8e5217010102625
Tom Ogle <tom.ogle@justice.gov.uk>
2025-12-19 16:03:01 +0000

    DHS-555: Improve names

--- modules/dms_s3_v2/metrics.tf
+++ modules/dms_s3_v2/metrics.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_metric_filter" "dms_replication_instance_errors" {
   count = var.setup_dms_instance ? 1 : 0
 
-  name           = "DMSTaskErrors"
+  name           = "DMSReplicationInstanceErrors"
   log_group_name = aws_cloudwatch_log_group.dms-instance-log-group[0].name
   # Patterns:
   # ]E: is the internal error marker, the equivalent of ERROR: or FATAL: in log4j and similar
@@ -21,7 +21,7 @@ resource "aws_cloudwatch_log_metric_filter" "dms_replication_instance_errors" {
 resource "aws_cloudwatch_log_metric_filter" "dms_replication_instance_warnings" {
   count = var.setup_dms_instance ? 1 : 0
 
-  name           = "DMSTaskWarnings"
+  name           = "DMSReplicationInstanceWarnings"
   log_group_name = aws_cloudwatch_log_group.dms-instance-log-group[0].name
   # Patterns:
   # ]W: is the internal DMS warning marker, the equivalent of WARN: in log4j and similar
