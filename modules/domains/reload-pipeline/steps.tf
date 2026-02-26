d1988d8c240da15ca6d8332ad93cd94f6b98a700
Tom Ogle <tom.ogle@justice.gov.uk>
2026-02-26 15:26:16 +0000

    DHS-627: Do full compactions during ingestion and reload to support liquid clustering.

--- modules/domains/reload-pipeline/steps.tf
+++ modules/domains/reload-pipeline/steps.tf
@@ -596,7 +596,8 @@ locals {
           "--dpr.maintenance.root.path" : var.s3_structured_path,
           "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
           "--dpr.read.config.from.s3" : tostring(var.file_transfer_in),
-          "--dpr.config.key" : var.domain
+          "--dpr.config.key" : var.domain,
+          "--dpr.maintenance.full.compaction": "true"
         },
         "NumberOfWorkers" : var.compaction_structured_num_workers,
         "WorkerType" : var.compaction_structured_worker_type
@@ -636,7 +637,8 @@ locals {
           "--dpr.maintenance.root.path" : var.s3_curated_path,
           "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
           "--dpr.read.config.from.s3" : tostring(var.file_transfer_in),
-          "--dpr.config.key" : var.domain
+          "--dpr.config.key" : var.domain,
+          "--dpr.maintenance.full.compaction": "true"
         },
         "NumberOfWorkers" : var.compaction_curated_num_workers,
         "WorkerType" : var.compaction_curated_worker_type
